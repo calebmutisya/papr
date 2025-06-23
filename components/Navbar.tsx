a7e6eb721e07ba4b65cc1627d62cfe4b4509abd6
calebmutisya <mutisyacalebmusau@gmail.com>
2025-06-23 17:47:00 +0300

    SearchBox done

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,5 +1,6 @@
+"use client"
 import Image from 'next/image';
-import React from 'react'
+import React, {useState} from 'react'
 import Link from 'next/link';
 
 import { FaFacebook } from "react-icons/fa";
@@ -9,9 +10,16 @@ import {FaLinkedin} from "react-icons/fa";
 
 import { CiSearch } from "react-icons/ci";
 import { CiMenuBurger } from "react-icons/ci";
-
+import { IoMdClose } from "react-icons/io";
 
 function Navbar() {
+
+    const [openSearchBox, setOpenSearchBox] = useState(false);
+
+    function toggleSearchBox() {
+        setOpenSearchBox(!openSearchBox);
+    }
+
   return (
     <header>
         {/* Top Bar */}
@@ -25,7 +33,7 @@ function Navbar() {
                     <li className='mr-[20px] hover:text-mypink transition ease-in duration-300'>About</li>
                     <li className='hover:text-mypink transition ease-in duration-300'>Contact</li>
                 </ul>
-                <ul className='text-textlight flex flex-row items-center px-[15px] '>
+                <ul className='text-mywhite flex flex-row items-center px-[15px] '>
                     <li className='ml-[20px] p-[5px] hover:text-mypink transition ease-in duration-300'><FaFacebook size={16}/></li>
                     <li className='ml-[20px] p-[5px] hover:text-mypink transition ease-in duration-300'><FaTwitter size={16}/></li>
                     <li className='ml-[20px] p-[5px] hover:text-mypink transition ease-in duration-300'><FaInstagram size={16}/></li>
@@ -34,48 +42,69 @@ function Navbar() {
             </div>
         </div>
         <div className=' border-solid border-1  border-b border-textlight'>
-            <nav className='container py-[22px] lg:py-[0px] flex justify-between items-center'>
-                <div className='flex flex-row items-center'>
-                    <Link href="/">
-                        <Image src="/logo-black.svg" alt="logo" width={102} height={34}/>
-                    </Link>
-                    <ul className='navlinks hidden lg:flex flex-row items-center pl-[44px] '>
-                        <li className='font-[500] mr-[34px] py-[30px] px-[5px] '>
-                            <Link href="/" className='hover:underline-animate'>
-                                Home
-                            </Link>
-                        </li>
-                        <li className='font-[500] mr-[34px] py-[30px] px-[5px]'>
-                            <Link href="/category/fashion" className='hover:underline-animate'>
-                                Fashion
-                            </Link>
-                        </li>
-                        <li className='font-[500] mr-[34px] py-[30px] px-[5px]'>
-                            <Link href="/category/lifestyle" className='hover:underline-animate'>
-                                Lifestyle
-                            </Link>
-                        </li>
-                        <li className='font-[500] mr-[34px] py-[30px] px-[5px]'>
-                            <Link href="/category/technology" className='hover:underline-animate'>
-                                Technology
-                            </Link>
-                        </li>
-                        <li className='font-[500] mr-[34px] py-[30px] px-[5px]'>
-                            <Link href="/category/sports" className='hover:underline-animate'>
-                                Sports
-                            </Link>
-                        </li>
-                    </ul>
-                </div>
+            <div className='relative'>
+                <nav className='container  py-[22px] lg:py-[0px] flex justify-between items-center'>
+                    <div className='flex flex-row items-center'>
+                        <Link href="/">
+                            <Image src="/logo-black.svg" alt="logo" width={102} height={34}/>
+                        </Link>
+                        <ul className='navlinks hidden lg:flex flex-row items-center pl-[44px] '>
+                            <li className='font-[500] mr-[34px] py-[30px] px-[5px] '>
+                                <Link href="/" className='hover:underline-animate'>
+                                    Home
+                                </Link>
+                            </li>
+                            <li className='font-[500] mr-[34px] py-[30px] px-[5px]'>
+                                <Link href="/category/fashion" className='hover:underline-animate'>
+                                    Fashion
+                                </Link>
+                            </li>
+                            <li className='font-[500] mr-[34px] py-[30px] px-[5px]'>
+                                <Link href="/category/lifestyle" className='hover:underline-animate'>
+                                    Lifestyle
+                                </Link>
+                            </li>
+                            <li className='font-[500] mr-[34px] py-[30px] px-[5px]'>
+                                <Link href="/category/technology" className='hover:underline-animate'>
+                                    Technology
+                                </Link>
+                            </li>
+                            <li className='font-[500] mr-[34px] py-[30px] px-[5px]'>
+                                <Link href="/category/sports" className='hover:underline-animate'>
+                                    Sports
+                                </Link>
+                            </li>
+                        </ul>
+                    </div>
+                    
+                    <div className='flex flex-row items-center'>
+                        <CiSearch size={24} className='mr-[20px] cursor-pointer hover:text-mypink' onClick={toggleSearchBox}/>
+                        <div className='py-[8px] px-[10px] hover:bg-mylightgrey transition ease-in duration-300 cursor-pointer rounded-full'>
+                            <CiMenuBurger size={24} />
+                        </div>   
+                    </div>
+                    
+                </nav>
                 
-                <div className='flex flex-row items-center'>
-                    <CiSearch size={24} className='mr-[20px] cursor-pointer hover:text-mypink'/>
-                    <div className='py-[8px] px-[10px] hover:bg-mylightgrey transition ease-in duration-300 cursor-pointer rounded-full'>
-                        <CiMenuBurger size={24} />
-                    </div>   
+                {/* Search Box */}
+                <div
+                className={`absolute top-0 left-0 w-full bg-mylightgrey py-[21px] z-10 transition-all duration-600 ${
+                    openSearchBox ? 'opacity-100 translate-y-0' : 'opacity-0 -translate-y-4 pointer-events-none'
+                }`}
+                >
+                    <div className='container flex flex-row items-center'>
+                        <input
+                        placeholder='Search Here ..'
+                        className='w-full py-[10px] px-[20px] text-[14px] outline-none border-b-1 border-myblack hover:border-mypink'
+                        />
+                        <div className='mr-[20px]'>
+                        <IoMdClose size={24} className='hover:text-mypink cursor-pointer' onClick={toggleSearchBox} />
+                        </div>
+                    </div>
                 </div>
-            </nav>
+            </div>
         </div>
+        
     </header>
   )
 }
